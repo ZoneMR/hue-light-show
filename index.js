800b5e48cb52d63293fc1f80909658bade54c5e7
Martin Rosinski <martin@zone-mr.net>
2016-12-07 16:32:48 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,172 @@
+//Config
+var HUE_HOST = '192.168.1.210';
+var HUE_USERNAME = '1473ccbe42fd33391f37807d1795be0d';
+var SHOW_FILE = process.argv[2];
+
+//Dependencies
+var ip = require("ip");
+var fs = require('fs');
+var airplayer = require('airplayer')
+var hue = require("node-hue-api");
+var ipAddress = ip.address();
+
+console.log(`<HueLightShow> on ${ipAddress}`);
+
+//Hue Lights Setup
+var HueApi = hue.HueApi;
+var HueLightState = hue.lightState;
+
+//Hue API
+hueApi = new HueApi(HUE_HOST, HUE_USERNAME);
+
+/*var displayStatus = function(status) {
+    console.log(JSON.stringify(status, null, 2));
+};
+
+hueApi.getFullState().then(displayStatus).then(process.exit).done();*/
+
+//Load and parse our Script
+var script = fs.readFileSync(SHOW_FILE, 'utf8') + '\n@end';
+
+if(!script) {
+	console.log("Couldn't load " + SHOW_FILE);
+}
+
+var actionsRegex = /([\s\S]*?)\B@(\S+)/g;
+var actions = {};
+var nextTime = undefined;
+
+while ((action = actionsRegex.exec(script)) !== null) {
+    var actionJs = action[1].trim();
+
+    if (!nextTime) {
+        console.log("Running Light Show Script Setup");
+        eval(actionJs);
+    } else {
+        actions[nextTime] = actionJs;
+    }
+
+    var actionTime = action[2].split('b');
+
+    nextTime = parseFloat(actionTime[0]);
+
+    if (actionTime[1]) {
+        nextTime += parseFloat(actionTime[1]) * BEAT_DURATION;
+    }
+}
+
+//console.log(actions);
+
+//Function which runs our light script
+var startTime;
+var lagCompensation = 0;
+
+function runScript() {
+    var lastAction = 0;
+    var actionTimes = Object.keys(actions).sort((a, b) => {
+        return parseFloat(a) - parseFloat(b);
+    });
+
+    console.log(actionTimes);
+
+    var interval = setInterval(() => {
+        var tickTime = process.hrtime(startTime);
+
+        tickTime = tickTime[0] + tickTime[1] / 1000000000;
+        tickTime += lagCompensation;
+
+        var actionTime = actionTimes[0];
+
+        if (!actionTime) {
+            console.log("Done all actions :)");
+
+            clearInterval(interval);
+        }
+
+        if (tickTime >= actionTime) {
+            var actionJs = actions[actionTime];
+
+            actionJs = actionJs.replace(/^\/\/#.*/gm, (val) => {
+                val = val.replace('\'', '\\\'');
+
+                return `console.log('${val}');`;
+            });
+
+            console.log(actionJs);
+
+            eval(actionJs);
+
+            actionTimes.shift();
+        }
+
+        //console.log(actionTime);
+        //console.log(tickTime);
+    }, 50);
+}
+
+//HTTP Server for AirPlay
+var express = require('express');
+var app = express();
+
+app.use(express.static('./assets/'));
+
+app.listen(8087);
+console.log('Listening on port 8087');
+
+//AirPlay the track
+var list = airplayer()
+
+list.on('update', function(player) {
+    console.log('Found new AirPlay device:', player.name)
+
+    if (player.name == AIRPLAY_TARGET) {
+        var audioFileURI = encodeURIComponent(AUDIO_FILE);
+        var file = `http://${ipAddress}:8087/${audioFileURI}`;
+        var statusInterval;
+
+        player.play(file, 0);
+        console.log('... Playing', file);
+
+        player.on('event', (data) => {
+            console.log('Event: ' + JSON.stringify(data));
+
+            if (data.state == "playing") {
+                startTime = process.hrtime();
+
+                runScript();
+
+                statusInterval = setInterval(() => {
+                    //Measure our lag at various intervals so we keep sync
+                    player.playbackInfo(function(err, res, body) {
+                        if (err) throw err
+                        //console.log('Playback info:', body)
+
+                        var airplayTime = body.position;
+
+                        var localTime = process.hrtime(startTime);
+                        localTime = localTime[0] + localTime[1] / 1000000000;
+
+                        var lag = airplayTime - localTime - lagCompensation;
+
+                        var capped_lag = lag;
+                        if(capped_lag > 0.05) capped_lag = 0.05;
+                        if(capped_lag < -0.05) capped_lag = -0.05;
+
+                        lagCompensation += capped_lag;
+
+                        if(lag > 0.01 || lag < -0.01) {
+                            console.log(`*** lag: ${lag} lagCompensation: ${lagCompensation}`);
+                        }
+                    })
+                }, 2.5 * 1000);
+            }
+
+            if (data.state == "stopped") {
+                player.destroy();
+                list.destroy();
+                clearInterval(statusInterval);
+                statusInterval = undefined;
+            }
+        });
+    }
+})
